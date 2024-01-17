800bcc36ef7604c8513e36400489a0057edc9e90
Alejandro Mery <amery@jpi.io>
2024-01-17 18:09:25 +0000

    Add dummy cobra-based go-gitmod command
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-gomod",
+	Short: "Manage gomod tags",
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
